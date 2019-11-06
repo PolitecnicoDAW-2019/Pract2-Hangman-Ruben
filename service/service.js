bb1c168f41faf5b6e41ab21ad5dbaf08f2d86b78
Rubén Cruz <55450777+rubencfu@users.noreply.github.com>
2019-11-06 08:46:02 +0100

    Add files via upload

--- service/service.js
+++ service/service.js
@@ -1,5 +1,5 @@
 class Service {
   checkWord = (letter, word) => word.toUpperCase().includes(letter);
 
-  checkHits = (hits, word) => hits === word.length;
+  checkHits = (hits, word) => hits === Array.from(new Set(word.toUpperCase())).join('').length;
 }
